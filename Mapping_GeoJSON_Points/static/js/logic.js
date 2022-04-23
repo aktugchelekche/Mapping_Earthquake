5df410d664c54cd8491c68268e258cf3fef4667e
AktugChelekche <aktugcilekci@hotmail.com>
2022-04-23 17:04:44 -0500

    geoJSON

--- Mapping_GeoJSON_Points/static/js/logic.js
+++ Mapping_GeoJSON_Points/static/js/logic.js
@@ -4,7 +4,7 @@ console.log("working");
 
 
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}',
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v10/tiles/{z}/{x}/{y}?access_token={accessToken}',
  {attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
@@ -36,7 +36,15 @@ let sanFranAirport =
 let map = L.map('mapid').setView([37.5, -122.5], 10);
 
 // Grabbing our GeoJSON data.
-L.geoJSON(sanFranAirport).addTo(map);
+// Grabbing our GeoJSON data.
+L.geoJSON(sanFranAirport, {
+    // We turn each feature into a marker on the map.
+    onEachFeature: function(feature, layer) {
+      console.log(layer);
+      layer.bindPopup("<h2> Airport Code : " + feature.properties.faa + " </h2> <hr> <h3>Airport Name :" + feature.properties.name + "<h3></h2>");
+    }
+
+  }).addTo(map);
 
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
